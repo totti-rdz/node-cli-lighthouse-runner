006b24c8eecb7e4ca2ea840cca3784fcb6b1b9e7
totti-rdz <thorsten_herfurtner@gmx.de>
2023-08-23 22:28:10 +0200

    use commander to accept arguments on the command line

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
 #!/usr/bin/env node
+import { Command } from 'commander';
 
-console.log('hello world');
+async function run() {
+  const program = new Command();
+
+  program
+    .argument('<url>', 'Lighthouse will run the analysis on the URL.')
+    .option(
+      '-i, --iteration <type>',
+      'How many times Lighthouse should run the analysis per URL',
+      '5'
+    )
+    .parse();
+
+  const [url] = program.args;
+  const options = program.opts();
+
+  console.log(`url: ${url}, iteration: ${options.iteration}`);
+}
+
+run();
