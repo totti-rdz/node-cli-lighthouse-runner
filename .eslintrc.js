1ff86c07d05e993c9bc817d4145620e268112f41
totti-rdz <thorsten_herfurtner@gmx.de>
2023-08-23 21:04:22 +0200

    add prettier plugin to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 // .eslintrc.js
 
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    extends: ['plugin:@typescript-eslint/recommended'],
-  }
-  
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+};
