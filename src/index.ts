08755be1b2530749217bac047db326f643fc7fcc
totti-rdz <thorsten_herfurtner@gmx.de>
2023-08-24 03:41:27 +0200

    extract formatting errors and score to Format class

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,7 @@ import spawn from 'cross-spawn';
 import { createRequire } from 'node:module';
 import { computeMedianRun } from 'lighthouse/core/lib/median-run.js';
 import chalk from 'chalk';
-import { formatScore } from './utils/formatScore.js';
-import os from 'os';
+import { format } from './utils/format.js';
 
 const require = createRequire(import.meta.url);
 
@@ -13,9 +12,6 @@ const lighthouse = require.resolve('lighthouse/cli');
 
 async function run() {
   const program = new Command();
-
-  const styleError = (message: string) =>
-    chalk.bold.redBright(message + os.EOL);
 
   program
     .argument('<url>', 'Lighthouse will run the analysis on the URL.')
@@ -25,7 +21,7 @@ async function run() {
       '5'
     )
     .configureOutput({
-      outputError: (str, write) => write(styleError(str)),
+      outputError: (str, write) => write(format.error(str)),
     })
     .parse();
 
@@ -33,7 +29,7 @@ async function run() {
   const options = program.opts();
 
   if (!(url.startsWith('http://') || url.startsWith('https://'))) {
-    console.error(styleError('Url must start with "http://" or "https://"'));
+    console.error(format.error('Url must start with "http://" or "https://"'));
     return;
   }
 
@@ -59,7 +55,7 @@ async function run() {
   }
 
   if (!results || results.length < 1) {
-    console.error(styleError('Running lighthouse yielded no results'));
+    console.error(format.error('Running lighthouse yielded no results'));
     return;
   }
 
@@ -67,7 +63,7 @@ async function run() {
 
   console.log(`\n${chalk.green('✔')} Report is ready for ${median.finalUrl}`);
   console.log(
-    `🗼 Median performance score: ${formatScore(
+    `🗼 Median performance score: ${format.score(
       median.categories.performance.score,
       median.categories.performance.score * 100
     )}`
@@ -84,7 +80,7 @@ async function run() {
 
   primaryMatrices.map((matrix) => {
     const { title, displayValue, score } = median.audits[matrix];
-    console.log(`🗼 Median ${title}: ${formatScore(score, displayValue)}`);
+    console.log(`🗼 Median ${title}: ${format.score(score, displayValue)}`);
   });
 }
 
---
+++ src/utils/format.ts
@@ -0,0 +1,34 @@
+import os from 'os';
+import chalk from 'chalk';
+import { styleError } from './styleError.js';
+
+class Format {
+  public error(message: string) {
+    return chalk.bold.redBright(message + os.EOL);
+  }
+
+  public score(score: number, value: number) {
+    /**
+     * Coloring display value based on Lighthouse score.
+     *
+     * - 0 to 0.49 (red): Poor
+     * - 0.5 to 0.89 (orange): Needs Improvement
+     * - 0.9 to 1 (green): Good
+     */
+    if (score < 0 || score > 1) {
+      throw new Error(styleError('Score out of bound'));
+    }
+
+    if (score >= 0.9 && score <= 1) {
+      return chalk.green(`${value} (Good)`);
+    }
+
+    if (score >= 0.5 && score < 0.9) {
+      return chalk.yellow(`${value} (Needs Improvement)`);
+    }
+
+    return chalk.red(`${value} (Poor)`);
+  }
+}
+
+export const format = new Format();
