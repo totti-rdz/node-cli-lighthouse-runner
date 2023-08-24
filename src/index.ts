14715fb6ad54215959e60c0b8a2c64bec559289b
totti-rdz <thorsten_herfurtner@gmx.de>
2023-08-24 12:39:17 +0200

    move valid url guard clause to cli.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
 #!/usr/bin/env node
-import { handleError } from './utils/handleError.js';
+import { cli } from './services/cli.js';
 import { runLightHouse } from './services/runLighthouse.js';
 import { resultHandler } from './services/resultHandler.js';
-import { cli } from './services/cli.js';
 
 async function run() {
   const { url, options } = cli();
-
-  if (!(url.startsWith('http://') || url.startsWith('https://'))) {
-    handleError('Url must start with "http://" or "https://"');
-  }
 
   const results = runLightHouse(url, options);
 
--- src/services/cli.ts
+++ src/services/cli.ts
@@ -1,5 +1,6 @@
 import { Command } from 'commander';
 import { format } from '../utils/format.js';
+import { handleError } from '../utils/handleError.js';
 
 export const cli = () => {
   const program = new Command();
@@ -18,6 +19,10 @@ export const cli = () => {
 
   const [url] = program.args;
   const options = program.opts();
+
+  if (!(url.startsWith('http://') || url.startsWith('https://'))) {
+    handleError('Url must start with "http://" or "https://"');
+  }
 
   return { url, options };
 };
