9ae48a85b4abc3c1d3760ee1a1d00903f977adf7
totti-rdz <thorsten_herfurtner@gmx.de>
2023-08-24 02:01:32 +0200

    prettier

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import { Command } from 'commander';
 import spawn from 'cross-spawn';
 import { createRequire } from 'node:module';
-import {computeMedianRun} from 'lighthouse/core/lib/median-run.js'
+import { computeMedianRun } from 'lighthouse/core/lib/median-run.js';
 import chalk from 'chalk';
 import { draw } from './utils/printScore.js';
 
@@ -46,29 +46,29 @@ async function run() {
     results.push(JSON.parse(stdout.toString()));
   }
 
-const median = computeMedianRun(results)
+  const median = computeMedianRun(results);
 
-console.log(`\n${chalk.green('✔')} Report is ready for ${median.finalUrl}`)
-console.log(
-  `🗼 Median performance score: ${draw(
-    median.categories.performance.score,
-    median.categories.performance.score * 100
-  )}`
-)
+  console.log(`\n${chalk.green('✔')} Report is ready for ${median.finalUrl}`);
+  console.log(
+    `🗼 Median performance score: ${draw(
+      median.categories.performance.score,
+      median.categories.performance.score * 100
+    )}`
+  );
 
-const primaryMatrices = [
-  'first-contentful-paint',
-  'interactive',
-  'speed-index',
-  'total-blocking-time',
-  'largest-contentful-paint',
-  'cumulative-layout-shift',
-]
+  const primaryMatrices = [
+    'first-contentful-paint',
+    'interactive',
+    'speed-index',
+    'total-blocking-time',
+    'largest-contentful-paint',
+    'cumulative-layout-shift',
+  ];
 
-primaryMatrices.map((matrix) => {
-  const { title, displayValue, score } = median.audits[matrix]
-  console.log(`🗼 Median ${title}: ${draw(score, displayValue)}`)
-})
+  primaryMatrices.map((matrix) => {
+    const { title, displayValue, score } = median.audits[matrix];
+    console.log(`🗼 Median ${title}: ${draw(score, displayValue)}`);
+  });
 }
 
 run();
